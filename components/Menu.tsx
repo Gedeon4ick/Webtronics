025191d5ce9bf23001be2ad00fdee1cc11a4548c
Gedeon4ick <pavell720@gmaul.com>
2023-01-23 17:56:45 +0600

    second commit

---
+++ components/Menu.tsx
@@ -0,0 +1,34 @@
+import styles from "../styles/Menu.module.scss"
+import Image from 'next/image'
+import Logo from "../icons/Logo.png"
+
+type Link = {
+    id: number;
+    name: string;
+    href: string;
+}
+
+const Menu = ({ links }: {links: Link[]}): JSX.Element => {
+    return (
+            <div className={styles.menu}>
+                <div className={styles.img}>
+                    <Image src={Logo} alt="logo" />
+                </div>
+                <ul className={styles.ul}>
+                    {links.map(link => 
+                        <li className={styles.li} key={link.id}>
+                            <a className={styles.a} href={link.href}>{link.name}</a>
+                        </li>
+                    )}
+                </ul>
+            </div>
+    )
+};
+
+
+export default Menu;
+
+
+
+
+
