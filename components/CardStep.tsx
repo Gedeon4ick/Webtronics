f9c571d46f087a5f406e66ae30e08af56e3d0c80
Gedeon4ick <pavell720@gmaul.com>
2023-01-24 20:06:53 +0600

    border

---
+++ components/CardStep.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/Steps.module.scss'
+
+const CardStep = () => {
+    return (
+        <div className={styles.next}>
+            <div className={styles.wrapper}>
+            <div className="title">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, amet minus at reprehenderit vero, atque modi maiores iure molestias repudiandae sed libero mollitia neque enim, voluptas sunt. Unde, illo accusantium.
+            </div>
+            <div className="subtitile">
+
+            </div>
+            <div className="descr">
+
+            </div>
+        </div>
+        </div>
+        
+    );
+};
+
+export default CardStep;
--- components/Steps.tsx
+++ components/Steps.tsx
@@ -1,9 +1,12 @@
+import styles from '../styles/Steps.module.scss'
+import CardStep from "./CardStep";
 import TitleH2 from "./TitleH2";
 
 const Steps = () => {
     return (
-        <div>
+        <div className={styles.steps}>
             <TitleH2 title={"Steps"}/>
+            <CardStep/>
         </div>
     );
 };
