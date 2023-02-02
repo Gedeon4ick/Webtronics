1517dd315a3f58f962e801d928f66942774a2c02
Gedeon4ick <pavell720@gmaul.com>
2023-02-02 14:52:39 +0600

    fix yup

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({ home }: InferGetStaticPropsType<typeof getStaticP
 				<title>Webtronics</title>
 				<meta name="keywords" content="Webtronics,next.js,react" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
 			</Head>
 			<header className={styles.header}>
